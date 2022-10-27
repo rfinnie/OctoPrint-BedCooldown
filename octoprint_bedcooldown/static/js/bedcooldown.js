ea2c2fda408c9fe37ad2d3c468c8a62147bb82a6
Ryan Finnie <ryan@finnie.org>
2022-10-26 22:39:23 -0700

    Use full view model for dummy gettext

--- octoprint_bedcooldown/static/js/bedcooldown.js
+++ octoprint_bedcooldown/static/js/bedcooldown.js
@@ -1,8 +1,16 @@
 $(function() {
-    // Dummy translation requests for dynamic strings supplied by the backend
-    // noinspection BadExpressionStatementJS
-    [
-        // mark labels
-        gettext("Cooldown")
-    ];
-}
+    function BedCooldownViewModel(parameters) {
+        // Dummy translation requests for dynamic strings supplied by the backend
+        // noinspection BadExpressionStatementJS
+        [
+            // mark labels
+            gettext("Cooldown")
+        ];
+    };
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: BedCooldownViewModel,
+        dependencies: ["temperatureViewModel"],
+        elements: []
+    });
+});
